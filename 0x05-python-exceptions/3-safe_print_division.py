d6175dea4d65116ae4e1b85c85b256043f308485
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-25 10:29:51 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         rslt = a / b
-    except:
+    except ZeroDivisionError:
         rslt = None
     finally:
         print("Inside result: {}".format(rslt))
