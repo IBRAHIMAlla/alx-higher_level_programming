d9cca4e567123c99075a0aa8cfc5fd8b85b735f6
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-25 14:45:38 -0700

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    rslt = 0
+    for m in range(1, 3):
+        try:
+            if m > a:
+                raise Exception("Too far")
+            rslt += (a ** b) / m
+        except Exception:
+            rslt = b + a
+            break
+    return rslt
