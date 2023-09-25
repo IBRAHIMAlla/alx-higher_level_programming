b62c44aa9d51b2ada4d5fb2b19babd91620737ff
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-25 14:22:42 -0700

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 import sys
 
 
 def safe_function(fct, *args):
+
 	try:
 		rslt = fct(*args)
-	except Exception:
-		rslt = None
+		return (rslt)
+	except:
 		print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-	finally:
-		return rslt
+		return (None)
