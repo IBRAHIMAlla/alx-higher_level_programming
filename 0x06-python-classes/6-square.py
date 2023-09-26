13dc7f57a4b981cc6e2260abdd933f7a434db47d
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-26 11:35:24 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """  class defined
+    """ Represent class
         Attributes:
             size (int): Size of square
             position (tuple): position of space and new lines
