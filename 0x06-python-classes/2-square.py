8b2739f9eccf86be1a3f8f73b141dc0a40dd5957
IBRAHIMAlla <brahimalla973@gmail.com>
2023-09-26 10:34:26 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""class Square """
+""" creates class Square """
 
 
 class Square:
+
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
