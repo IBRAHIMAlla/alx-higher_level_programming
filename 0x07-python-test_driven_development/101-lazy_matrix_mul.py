c74ec3e0b53e30e8595c60a9b2a7733b2a1b171e
IBRAHIMAlla <brahimalla973@gmail.com>
2023-10-03 15:04:06 -0700

    101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Describes the lazy_matrix function.
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Performs the matrix multiplication operation on two matrices"""
+    return numpy.matmul(m_a, m_b)
