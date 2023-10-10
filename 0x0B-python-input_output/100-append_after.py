43b6b34804f9fdb6d7494d4fdaa73e8fe375ff6c
IBRAHIMAlla <brahimalla973@gmail.com>
2023-10-10 10:22:02 -0700

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for append_after method.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''Method for inserting.'''
+    ln = []
+    with open(filename, "r", encoding="utf-8") as m:
+        ln = m.readlines()
+        y = 0
+        while y < len(ln):
+            if search_string in ln[y]:
+                ln[y:y + 1] = [ln[y], new_string]
+                y += 1
+            y += 1
+    with open(filename, "w", encoding="utf-8") as f:
+        m.writelines(ln)
