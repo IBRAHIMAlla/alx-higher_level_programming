b1abd8d616a12a469b997c5a79e13d988663ebaa
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-14 09:08:16 -0800

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
--- computes the score average of all records in the table second_table of the database hbtn_0c_0
-SELECT `score`, COUNT(`score`) 'number' FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
+-- computes the score average of all records in the table second_table of the database
+SELECT AVG(`score`) 'average' FROM second_table;
