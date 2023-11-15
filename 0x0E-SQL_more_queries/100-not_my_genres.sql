e1aa16303f2aa635f2c644e4bd492cbdd393114a
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-15 15:37:31 -0800

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,7 +4,7 @@ SELECT tv_g.name
 FROM tv_genres g
 WHERE tv_g.name NOT IN (
       SELECT tv_g.name
-      FROM tv_genres g
+      FROM tv_genres tv_g
       INNER JOIN tv_show_genres y ON tv_g.id = y.genre_id
       INNER JOIN tv_shows m ON y.show_id = m.id
       WHERE m.title = 'Dexter'
