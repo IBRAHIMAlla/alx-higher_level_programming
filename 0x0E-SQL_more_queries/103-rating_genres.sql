9f51667cb7b4794632bc016688dc024a3da0d6de
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-15 15:59:58 -0800

    103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+
+SELECT tv_genres.name, SUM(tv_show.rate) AS rating_sum
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY rating_sum DESC;
