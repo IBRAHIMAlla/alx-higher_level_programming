864d08e26e41709e4408fb02f37ad812ec46ff8e
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-15 16:07:31 -0800

    103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,6 +2,7 @@
 
 SELECT tv_genres.name, SUM(tv_shows.rating) AS rating_sum
 FROM tv_genres
-JOIN tv_shows ON tv_genres.id = tv_shows.genre_id
-GROUP BY tv_genres.name
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show ON tv_show_genres.show_id = tv_show.show_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY rating_sum DESC;
