a021e0439f0b648c59811add779f16e8b57c8c03
IBRAHIMAlla <brahimalla973@gmail.com>
2023-11-15 16:30:14 -0800

    102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title, SUM(ratings.rate) AS rating_sum
 FROM tv_shows
 INNER JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY ratings.show_id
+GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
