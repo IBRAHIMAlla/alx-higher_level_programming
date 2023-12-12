ac6066fd4f6ac3485915cb9c622cca2244aaba37
IBRAHIMAlla <brahimalla973@gmail.com>
2023-12-12 07:00:43 -0800

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const m = require('fs');
+
+const s1 = process.argv[2];
+const s2 = process.argv[3];
+const dt = process.argv[4];
+
+function callback (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  fs.appendFile(dt, data, function (err) {
+    if (err) console.log(err);
+  });
+}
+
+m.readFile(s1, 'utf8', callback);
+m.readFile(s2, 'utf8', callback);
