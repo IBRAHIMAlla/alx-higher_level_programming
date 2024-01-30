5c473a0ebca4b72e15f20d93c2d589a1fede0673
IBRAHIMAlla <brahimalla973@gmail.com>
2024-01-30 13:31:47 -0800

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,15 @@
 #!/uir/bin/node
 
-const request = require('request');
-const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const req = require('request');
+const ep = process.argv[2];
+const Au = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (err, response, body) {
+req(Au + ep, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
+    const resp = JSON.parse(body);
+    console.log(resp.title);
   } else {
     console.log('Error code: ' + response.statusCode);
   }
